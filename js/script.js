24ba0c85a36dac19d6988521288959782e8b6c3b
Giux <giuliamelody@gmail.com>
2020-07-14 16:00:00 +0200

    4 js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 
+function addSendListner(){
+ var target = $('#messaggio');
+ target.keyup(sendKeyUp);
+};
+
+function sendKeyUp(event){
+  var key = event.which;
+
+  if ( key === 13) {
+    var input = $(this);
+    var txt = input.val();
+
+    input.val('');
+
+    sendMessage(txt);
+  }
+
+};
+
+function sendMessage(txt){
+  var template = $('#template-messaggi-inviati > div').clone();
+  var target = $('#verde');
+
+  template.find('#testo').txt(txt);
+  template.find('#orario').text(getHour());
+
+  target.append(template);
+};
+
+function getHour(){
+  var ora = new Date();
+  return ora.getHours() + ':' + ora.getMinutes();
+};
+
+function init(){
+
+  addSendListner();
+
+};
+
+$(document).ready(init);
